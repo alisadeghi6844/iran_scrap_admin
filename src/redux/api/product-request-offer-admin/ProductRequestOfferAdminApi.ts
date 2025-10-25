2ad6273b295663cf851c651cc7be86bd7fcf1886
alisadeghi <alisadeghishiaomi@gmail.com>
2025-10-25 11:57:47 +0330

    fixed

--- src/redux/api/product-request-offer-admin/ProductRequestOfferAdminApi.ts
+++ src/redux/api/product-request-offer-admin/ProductRequestOfferAdminApi.ts
@@ -1,6 +1,6 @@
 export const CLOSE_REQUEST_POINT = "product-request-offer-admin/test/close-request";
 export const EXPIRE_OFFER_POINT = "product-request-offer-admin/test/expire-offer";
-export const GET_PRODUCT_REQUEST_OFFER_ADMIN_POINT = "product-request-offer-admin";
+export const GET_PRODUCT_REQUEST_OFFER_ADMIN_POINT = "product-request-offer/ali";
 export const GET_PRODUCT_REQUEST_OFFER_ADMIN_BY_ID_POINT = "product-request-offer-admin";
 export const GET_PRODUCT_REQUEST_OFFERS_BY_REQUEST_ID_POINT = "product-request-offer-admin/by-request";
 export const VERIFY_PAYMENT_POINT = "product-request-offer-admin/confirmed-offer/verify-payment";
